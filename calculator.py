c45fba1a1e6f5c93aa5f0923903bc9dc25c6cfe6
Portia <portiamasetla20@gmail.com>
2026-01-24 23:40:01 +0200

    fix: make my functins operate

--- calculator.py
+++ calculator.py
@@ -1,12 +1,37 @@
-def calc(a, b):
-   ...
-   # Addition
+# Addition
+def add(a, b):
+   value = int(input("Enter values to get their sum: (E.g. 3 + 2) "))
+   print(value)
+   return a + b
+   pass
 
+# Subtraction
+def subtract(a, b):
+   value = int(input("Enter values to get their difference: (E.g. 3 - 2) "))
+   print(value)
+   return a - b
+   pass
 
-   # Subtraction
+# Multiplication
+def multiply(a, b):
+   value = int(input("Enter values to get their multiple: (E.g. 3 * 2) "))
+   print(value)
+   return a * b
+   pass
 
+# Division
+def divide(a, b):
+   value = int(input("Enter values to get their qoutient: (E.g. 3 / 2) "))
+   print(value)
+   return a / b
+   pass
 
-   # Multiplication
+# def main():
+#    add(5, 5)
+#    subtract(10, 6)
+#    multiply(3, 3)
+#    divide(81, 9)
 
-
-   # Division
+if __name__ =="__main__":
+   # print(main())
+   add()
